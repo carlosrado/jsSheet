f34559873dcf49e7e67b61bbc669edb8fa37ed4d
carlosrado <43089893+carlosrado@users.noreply.github.com>
2023-05-28 23:09:02 +0200

    Update coolFunctions.js

--- coolFunctions.js
+++ coolFunctions.js
@@ -1,2 +1,19 @@
-// We map the num and his position, if  includes the target - num (sum for tarjet) return i else undefinded, then we filter only integers
-const twoSum = (nums, target) =>  nums.map((num, i) => nums.includes(target - num) ? i : undefined).filter((a) => Number.isInteger(a))
+/** TwoSum
+ * If there is a sum for the target return their indexex, only one solution and can't be with itself
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = (nums, target) => {
+	const counts = nums.reduce((acc, value) => ({ // count repeated elements of the array {'n':count}
+		...acc,
+		[value]: (acc[value] || 0) + 1
+	}), {});
+	const complements = nums.map(n => target-n) // array of the complements number for the sum
+	const numsStr = nums.map(n => n.toString()) // nums parsed to string
+	let indexes = nums.map((num, i) =>
+		//if nums includes complement AND (num is not the same as complemen OR there is repetead value return index, else return undefinde)
+		nums.includes(target - num) && (nums[i] !== complements[i] || counts[numsStr[i]] > 1) ? i : undefined)
+		.filter((a) => Number.isInteger(a)) //Return only the integers
+	return indexes
+}
